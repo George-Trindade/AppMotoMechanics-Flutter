1a3cb562489c9620d5bb67f4312621a76aa7c86c
George Trindade Rodrigues <george.rodrigues@estudante.ifto.edu.br>
2021-05-05 12:51:14 -0300

    Aula08-LinhasColunas

--- lib/ComEstado.dart
+++ lib/ComEstado.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 import 'Tema.dart';
 
+int cont = 0;
+
 class ComEstado extends StatefulWidget {
   @override
   State<ComEstado> createState() {
@@ -15,14 +17,59 @@ class Estado extends State<ComEstado> {
     return Scaffold(
       appBar: AppBar(
         title: Text("Page 02"),
+        actions: [
+          MeuSwitcher(),
+        ],
       ),
-      body: Center(
-        child: Switch(
-            value: Tema.instance.isThemeDark,
-            onChanged: (value) {
-              Tema.instance.changeTheme();
-            }),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Text('Contador: $cont'),
+          MeuSwitcher(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 25,
+                height: 25,
+                color: Colors.deepPurple,
+              ),
+              Container(
+                width: 25,
+                height: 25,
+                color: Colors.deepOrange,
+              ),
+              Container(
+                width: 25,
+                height: 25,
+                color: Colors.pinkAccent,
+              ),
+            ],
+          )
+        ]),
       ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          setState(() {
+            cont++;
+          });
+        },
+      ),
+    );
+  }
+}
+
+class MeuSwitcher extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Switch(
+      value: Tema.instance.isThemeDark,
+      onChanged: (value) {
+        Tema.instance.changeTheme();
+        cont--;
+      },
     );
   }
 }
